9ba74df3e51a527fc26be750f981316f9f46a6d0
Samuel Colares <samuelcolaresdeaquino@gmail.com>
2023-03-14 03:15:02 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,12 +3,16 @@ const $LEFTSIDE = document.querySelector('.left')
 const $RIGHTSIDE = document.querySelector('.right')
 const $RETURN_BUTTON = document.querySelector('.returnBtn')
 const $BUTTON = document.querySelectorAll('.btn')
+const $LEFTTEXT = document.querySelector('.left-info')
+const $RIGHTTEXT = document.querySelector('.right-info')
 
 
 
 $LEFTSIDE.addEventListener('mouseenter', () => {
     $CONTAINER.classList.add('hover-left')
-    $BUTTON[0].classList.add('show')
+    setTimeout(() => {
+        $BUTTON[0].classList.add('show')
+    }, 290)
     setTimeout(() => {
         $BUTTON[0].classList.add('opacity')
     }, 300)
@@ -17,14 +21,18 @@ $LEFTSIDE.addEventListener('mouseenter', () => {
 $LEFTSIDE.addEventListener('mouseleave', () => {
     $CONTAINER.classList.remove('hover-left')
     $BUTTON[0].classList.remove('show')
+    $BUTTON[0].classList.remove('opacity')
     setTimeout(() => {
+        $BUTTON[0].classList.remove('show')
         $BUTTON[0].classList.remove('opacity')
     }, 300)
 })
 
 $RIGHTSIDE.addEventListener('mouseenter', () => {
     $CONTAINER.classList.add('hover-right')
-    $BUTTON[1].classList.add('show')
+    setTimeout(() => {
+        $BUTTON[1].classList.add('show')
+    }, 290)
     setTimeout(() => {
         $BUTTON[1].classList.add('opacity')
     }, 300)
@@ -33,7 +41,9 @@ $RIGHTSIDE.addEventListener('mouseenter', () => {
 $RIGHTSIDE.addEventListener('mouseleave', () => {
     $CONTAINER.classList.remove('hover-right')
     $BUTTON[1].classList.remove('show')
+    $BUTTON[1].classList.remove('opacity')
     setTimeout(() => {
+        $BUTTON[1].classList.remove('show')
         $BUTTON[1].classList.remove('opacity')
     }, 300)
 })
@@ -41,8 +51,22 @@ $RIGHTSIDE.addEventListener('mouseleave', () => {
 $RETURN_BUTTON.addEventListener('click', () => {
     $CONTAINER.classList.remove('hover-right')
     $CONTAINER.classList.remove('hover-left')
-    $BUTTON[0].classList.remove('show')
-    $BUTTON[0].classList.remove('opacity')
-    $BUTTON[1].classList.remove('show')
-    $BUTTON[1].classList.remove('opacity')
 })
+
+
+$LEFTTEXT.addEventListener('mousedown', () => {
+    $LEFTTEXT.classList.add('opacityText')
+})
+$LEFTTEXT.addEventListener('mouseup', () => {
+    $LEFTTEXT.classList.remove('opacityText')
+})
+
+$RIGHTTEXT.addEventListener('mousedown', () => {
+    $RIGHTTEXT.classList.add('opacityText')
+})
+$RIGHTTEXT.addEventListener('mouseup', () => {
+    $RIGHTTEXT.classList.remove('opacityText')
+})
+
+
+document.querySelector('.videoD4').playbackRate = 1.5
